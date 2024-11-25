e456de320e0c2fe8e313e53c860a48e4cd4a0382
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-11-26 02:02:54 +0530

    adding middlewares to handle file

--- src/middlewares/file-upload.middleware.js
+++ src/middlewares/file-upload.middleware.js
@@ -4,12 +4,18 @@ import path from 'path';
 
 // now store files in the disk or local storage
 
-const storage= multer.diskStorage({
+const storageConfig= multer.diskStorage({
     destination:(req,file,cb) =>{
+        
         cb(null,'public/images/');
     },
     filename:(req,file,cb)=>{
         const name=Date.now()+file.originalname+path.extname(file.originalname);
         cb(null,name);
     }
+
+
+    export const uploadFile=multer({
+        storage:storageConfig
+    })
 })
